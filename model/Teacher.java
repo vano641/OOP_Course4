142c592642642c3c5a5da9669bb2147bd048b4a6
Ivan Mysovskii <vano641@mail.ru>
2024-11-24 19:34:19 +0300

    Наследник базового класса User

---
+++ model/Teacher.java
@@ -0,0 +1,21 @@
+public class Teacher extends User{
+    
+    private int teacherId;
+
+
+    public Teacher(String firstName, String lastName, String middleName, int teacherId) {
+        super(firstName, lastName, middleName);
+        this.teacherId = teacherId;
+    }
+
+
+    public int getTeacherId() {
+        return this.teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+
+}
