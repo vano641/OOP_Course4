81d9a72da8ae24556fe98f9d2a30d143c9c37ab4
Ivan Mysovskii <vano641@mail.ru>
2024-11-24 19:31:36 +0300

    Условие задачи №1

---
+++ main.java
@@ -0,0 +1,13 @@
+/**
+ * 1) Создать package - model. Работу продолжаем в нем
+ * 2) Реализовать абстрактный класс User и его наследники Student и Teacher.
+ * User имеет в себе общие данные (ФИО)
+ * Student и Teacher собственные параметры studentId и teacherId
+ */
+
+public class main {
+    
+    public static void main(String[] args) {
+        
+    }
+}
